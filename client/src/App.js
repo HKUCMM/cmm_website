d2aa5914d17d03cbfad930c00c90a85272370c0b
johnj09 <johnjangjaeho123@gmail.com>
2024-03-03 18:26:13 +0800

    Sample get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import react from React
+import React, { useEffect, useState } from 'react'
 
 function App() {
   
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/user").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
-    <div>Hello World!</div>
+    <div>
+      {(typeof backendData.users === "undefined") ? (
+        <p>Loading ...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+    </div>
   )
 
 }
