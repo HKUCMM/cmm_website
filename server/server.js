8a052d64244cdc3dc752f15a07b13f7496afc58a
jihoo529 <jihoolee529@gmail.com>
2024-03-12 22:18:04 +0800

    modified db connection

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use(express.json());
 app.use('/', user_router);
 app.use('/', content_router);
 
-var server = app.listen(5000, () => {
+var server = app.listen(8081, () => {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
