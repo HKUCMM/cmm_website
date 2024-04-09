2032e9baf100b759d31a74bdbefe1d4d138a0d52
LEE Yedam <leeyd04@connect.hku.hk>
2024-04-09 23:59:51 +0800

    overall changes (useState)

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -2,16 +2,82 @@ import React, { useEffect, useState } from "react";
 import "../style/login.css";
 
 const Login = () => {
+  const [inputFields, setInputFields] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+
+  const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  const validateValues = (inputValues) => {
+    let errors = {};
+    if (inputValues.email.length <= 0) {
+      errors.email = "Required field";
+      errors.emailEmpty = "Required field";
+    } else if (!inputValues.email.match(isValidEmail)) {
+      errors.email = "Invalid email address!";
+      errors.emailInvalid = "Invalid email address!";
+    }
+    if (inputValues.password.length <= 0) {
+      errors.password = "Required field";
+    }
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validateValues(inputFields));
+    setSubmitting(true);
+  };
+
+  const finishSubmit = () => {
+    console.log(inputFields);
+  };
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      finishSubmit();
+    }
+  }, [errors]);
+
   return (
     <div className="login">
       <div className="leftBox">
         <span className="leftHeader">LOG IN</span>
         <span className="leftWelcome">Welcome to CMM!</span>
       </div>
-      <form className="right">
+      <form className="right" onSubmit={handleSubmit}>
         <h1 className="rightHeader">Welcome Back!</h1>
-        <input className="input" type="text" placeholder="E-mail"></input>
-        <input className="input" type="password" placeholder="Password"></input>
+        <input
+          className={errors.email ? "falseInput" : "input"}
+          type="text"
+          name="email"
+          placeholder="E-mail"
+          value={inputFields.email}
+          onChange={handleChange}
+        ></input>
+        {errors.emailEmpty ? (
+          <p className="errorEmail">Required field</p>
+        ) : null}
+        {errors.emailInvalid ? (
+          <p className="errorEmail">Invalid email address!</p>
+        ) : null}
+        <input
+          className={errors.password ? "falseInput" : "input"}
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={inputFields.password}
+          onChange={handleChange}
+        ></input>
+        {errors.password ? (
+          <p className="errorPassword">Required field</p>
+        ) : null}
         <p className="forget">Forgot Password?</p> {/* 링크로 바꿔야 함 */}
         <button className="loginButton">Login</button>{" "}
         {/* 누르면 sign up 페이지로 */}
