52711ffb17be0af7ae496110b3396ecbbb15b3a1
siwookim00 <63380176+siwookim1114@users.noreply.github.com>
2024-05-30 02:16:22 +0900

    Finished Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,31 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import "./Navbar.css";
 
 function Mynav() {
   let navItem = ["ABOUT", "PROJECT", "NOTICE", "CONTACT US", "LOG IN"];
 
   return (
     <>
-      <Navbar
-        style={{ backgroundColor: "#1D2528" }}
-        bg="dark"
-        data-bs-theme="dark"
-      >
-        <Container>
+      <Navbar style={{ backgroundColor: "#1D2528" }} data-bs-theme="dark">
+        <Container fluid style={{ marginLeft: "60px", marginRight: "60px" }}>
           <Navbar.Brand href="#home">
-            <img src="/CMM_LOGO.png" alt="CMM Logo" width="80px" />
+            <img
+              src={process.env.PUBLIC_URL + "/CMM_LOGO.png"}
+              alt="CMM Logo"
+              width="80px"
+            />
           </Navbar.Brand>
-          <Nav className="ms-auto">
+          <Nav>
             {navItem.map(function (a) {
               return (
                 <Nav.Link
                   href={`#${a}`}
-                  className="nav-item"
                   style={{
                     paddingRight: "30px",
                     fontSize: "20px",
                   }}
                 >
-                  {`${a}`}
+                  <span className="navigation-link">{`${a}`}</span>
                 </Nav.Link>
               );
             })}
