21d29d17c2916aa9b0160e91df55f161af9c7a11
siwookim00 <63380176+siwookim1114@users.noreply.github.com>
2024-05-30 02:09:39 +0900

    Added background picture

--- client/src/components/Mainpage.js
+++ client/src/components/Mainpage.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './MiddleSection.css'
+
 const MiddlePage = () => {
     return (
-        <div className = "middle-section">
+        <div 
+        className = "middle-section"
+        style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/background_mainpage.png'})` }}
+        >
             <div className = "content">
                 <h1>CMM</h1>
                 <h2>THE UNIVERSITY OF HONG KONG</h2>
