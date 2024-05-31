2273b87b87ed851d14a6fad6fa1106f9b9519bf5
johnj09 <johnjangjaeho123@gmail.com>
2024-05-31 23:07:08 +0800

    Removed styled components from Footer.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,110 +1,50 @@
-import styled from "styled-components";
-
-//배경색 그레이20 (임의)
-const FooterBox = styled.div`
-  background: #747474;
-`;
-
-const TopBox = styled.div`
-  height: 312px;
-  top: 832px;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-`;
-
-const ListBox = styled.div`
-  font-size: 18px;
-  margin-top: 53px;
-  margin-bottom: 54px;
-  display: flex;
-  flex-direction: column;
-  height: 205px;
-`;
-
-const ContactBox = styled.div`
-  width: 325px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-//grey 임의
-const ListTitle = styled.span`
-  color: #bdbdbd;
-  padding-bottom: 85px;
-`;
-
-const ListItem = styled.dl`
-  list-style-type: none;
-  color: white;
-  line-height: 30px;
-`;
-
-const StyledLink = styled.a`
-  text-decoration: none;
-  color: inherit;
-`;
-
-const Copyright = styled.div`
-  height: 81px;
-  color: #ffffff;
-  font-family: Titillium Web;
-  font-size: 16px;
-  font-weight: 275;
-  line-height: 24px;
-  letter-spacing: 0em;
-  text-align: center;
-  border-top: 1px solid silver;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+import "../css/footer.css";
 
 const Footer = () => {
   return (
-    <FooterBox>
-      <TopBox>
-        <ListBox>
-          <ListTitle>CONTACT</ListTitle>
-          <ContactBox>
-            <ListItem style={{ color: "#bdbdbd" }}>
+    <div className="footer-box">
+      <div className="top-box">
+        <div className="list-box">
+          <span className="list-title">CONTACTS</span>
+          <div className="contact-box">
+            <dl className="list-item" style={{ color: "#bdbdbd" }}>
               <li>ADDRESS</li>
               <li>E-MAIL</li>
               <li>WEBSITE</li>
-            </ListItem>
-            <ListItem>
+            </dl>
+            <dl className="list-item">
               <li>Pok Fu Lam, Hong Kong</li>
               <li>hkucmm@gmail.com</li>
               <li>www.cmm.com</li>
-            </ListItem>
-          </ContactBox>
-        </ListBox>
-        <ListBox>
-          <ListTitle>SOCIAL</ListTitle>
-          <ListItem>
+            </dl>
+          </div>
+        </div>
+        <div className="list-box">
+          <span className="list-title">SOCIALS</span>
+          <dl className="list-item">
             <li>
-              <StyledLink
+              <a
+                className="styled-link"
                 href="https://www.linkedin.com/company/cmmhku/"
                 target="_blank"
               >
                 LINKEDIN
-              </StyledLink>
+              </a>
             </li>
             <li>
-              <StyledLink
+              <a
+                className="styled-link"
                 href="https://www.instagram.com/hku.cmm/"
                 target="_blank"
               >
                 INSTAGRAM
-              </StyledLink>
+              </a>
             </li>
-          </ListItem>
-        </ListBox>
-      </TopBox>
-      <Copyright>Copyright © 2024 CMM All rights reserved.</Copyright>
-    </FooterBox>
+          </dl>
+        </div>
+      </div>
+      <div className="copyright">Copyright © 2024 CMM All rights reserved.</div>
+    </div>
   );
 };
 
