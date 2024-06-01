e5d5d5e0e6294e41847e5c3ddeb51bb4d13d6414
johnj09 <johnjangjaeho123@gmail.com>
2024-06-01 21:12:31 +0800

    Added cors origin to environment variable

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
